signupagain/projects

feat(gallery): add the title of gallery entry page

layers/gallery/app/pages/gallery/[[id]].vue
@@ -5,11 +5,56 @@
 
 	definePageMeta({
 		layout: 'gallery-default',
+		validate(meta) {
+			if (meta.name !== 'gallery-id')
+				return {
+					statusCode: 404,
+					message: 'Page not found',
+					fatal: true,
+				}
+
+			return (
+				(meta.params?.id === undefined ?
+					true
+				:	typeof +meta.params.id === 'number') || {
+					statusCode: 404,
+					message: 'Page not found',
+					fatal: true,
+				}
+			)
+		},
+	})
+
+	const { data: page } = await useAsyncData('gallery-page', () =>
+		queryCollection('gallery').first(),
+	)
+
+	if (!page.value) {
+		throw createError({
+			statusCode: 404,
+			message: 'Page not found',
+			fatal: true,
+		})
+	}
+
+	useSeoMeta({
+		title: page.value?.seo.title || page.value?.title,
+		ogTitle: page.value?.seo.title || page.value?.title,
+		description: page.value?.seo.description || page.value?.description,
+		ogDescription: page.value?.seo.description || page.value?.description,
 	})
 </script>
 
 <template>
-	<div></div>
+	<div v-if="page">
+		<UPageHero
+			:title="page.title"
+			:description="page.description"
+			:ui="{
+				description: 'text-pretty',
+			}"
+		/>
+	</div>
 </template>
 
 <style lang="scss"></style>

signupagain/portfolio

layers/gallery/app/pages/gallery/[[id]].vue
@@ -5,11 +5,44 @@
 
 	definePageMeta({
 		layout: 'gallery-default',
+		validate(meta) {
+			const appConfig = useAppConfig()
+
+			if (meta.name !== 'gallery-id') return appConfig.error.notFound
+
+			return (
+				(meta.params?.id === undefined ?
+					true
+				:	typeof +meta.params.id === 'number') || appConfig.error.notFound
+			)
+		},
+	})
+
+	const { data: page } = await useFetch('/api/gallery')
+
+	if (!page.value) {
+		const appConfig = useAppConfig()
+		throw createError(appConfig.error.notFound)
+	}
+
+	useSeoMeta({
+		title: page.value?.seo.title || page.value?.title,
+		ogTitle: page.value?.seo.title || page.value?.title,
+		description: page.value?.seo.description || page.value?.description,
+		ogDescription: page.value?.seo.description || page.value?.description,
 	})
 </script>
 
 <template>
-	<main></main>
+	<main v-if="page">
+		<UPageHero
+			:title="page.title"
+			:description="page.description"
+			:ui="{
+				description: 'text-pretty',
+			}"
+		/>
+	</main>
 </template>
 
 <style lang="scss"></style>